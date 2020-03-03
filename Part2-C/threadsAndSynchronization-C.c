95b21630c5749bcc66976107c771c10a3908737a
Ben Tisserand <bentisserand@gmail.com>
2020-03-03 14:48:59 -0800

    worked on it more, still a few errors
    Signed-off-by: Ben Tisserand <bentisserand@gmail.com>

--- Part2-C/threadsAndSynchronization-C.c
+++ Part2-C/threadsAndSynchronization-C.c
@@ -11,26 +11,42 @@ use the pthread flag with gcc to compile this code
 ~$ gcc -pthread producer_consumer.c -o producer_consumer
 */
 
+sem_t mutex;
+sem_t full;
+sem_t empty;
+
 struct Job{
     int id;
     int length;
-}
+};
 
 struct threadArgs{
     int threadId;
-    struct Job buffer[];
-}
+    struct Buffer* buffer;
+};
 
-void *Producer(void, *threadid){
+struct Buffer{
+    int endPointer;
+    int startPointer;
+    int length;
+    struct Job buffer[];
+};
+
+void *Producer(struct Buffer *buffer){
     int nextId = 0;
     while(1){
         sem_wait(&empty);
         struct Job nextJob;
         nextJob.id = nextId++;
-        nextJob.length = 1;
-        requestQ(); /*request ID increases sequentially and have a random reuqest length*/
+        nextJob.length = 1; //CHANGE
+	sem_wait(&mutex);
+	printf("\nProducer: Created job ID %d of length %d seconds at time ",nextJob.id,nextJob.length);
+        buffer->buffer[buffer->endPointer] = nextJob; /*request ID increases sequentially and have a random request length*/
+	buffer->endPointer = (buffer->endPointer + 1)%buffer->length;
         sem_post(&full);
-        sem_post(&requestQMutex);
+        sem_post(&mutex);
+
+	printf("\nProducer: sleeping for %d seconds.",1); //CHANGE
         sleep(1); /*Sleep for a random short duration*/
     }
     
@@ -39,17 +55,20 @@ void *Producer(void, *threadid){
 }
 
 
-void *Consumer(void, *threadid){
+void Consumer(struct threadArgs *thread){
+    struct Job nullJob;
+    nullJob.id = -1;
     while(1){
-        sem_wait(full);
-        sem_wait(requestQMutex);
-        long tid = (long)threadid;
-        printf("Hello World! It's me, thread #%ld!\n", tid)
-        rc.busy;
-        requestQ(); /*Decrements requestQ*/
-        sleep(requestQ[i]); /*Sleep for length of requestLength*/
-        sem_post(empty);
-        sem_post(requestMutex);
+        sem_wait(&full);
+        sem_wait(&mutex);
+        struct Job currentJob = thread->buffer->buffer[thread->buffer->startPointer];
+	thread->buffer->buffer[thread->buffer->startPointer] = nullJob;
+	thread->buffer->startPointer = (thread->buffer->startPointer+1)%thread->buffer->length;
+	sem_post(&mutex);
+        printf("\nConsumer %d: assigned job ID %d of length %d seconds at time ", thread->threadId, currentJob.id, currentJob.length);
+        sleep(currentJob.length); /*Sleep for length of request length*/
+	printf("\nConsumer %d: completed job ID %d at time ",thread->threadId, currentJob.length);
+        sem_post(&empty);
     }
     pthread_exit(NULL);
 }
@@ -63,27 +82,30 @@ int main (int argc, char *argv[]){
     
     int requestLengthMax = M; /*Max request length*/
     struct Job requestQ[N]; /*Request queue of buffer size N*/
+    struct Buffer* buffer = malloc(sizeof(struct Buffer) + N*sizeof(struct Job));
+    buffer->startPointer = 0;
+    buffer->endPointer = 0;
+    buffer->length = N;
 
-    sem_t requestQMutex;
-    sem_t full;
-    sem_t empty;
-
-    sem_init(requestQMutex, 1, 0);
-    sem_init(full, 0, 0);
-    sem_init(empty, 0, N);
+    sem_init(&mutex, 0, 1);
+    sem_init(&full, 0, 0);
+    sem_init(&empty, 0, N);
 
 
     int rc;
     long t;
-    for(t=0, t<N; t++){ /*Create Consumer/Slave threads*/
+    for(t=0; t<N; t++){ /*Create Consumer/Slave threads*/
         printf("In main: creating thread %ld\n", t);
-        rc = pthread_create(&threads[t], Null, Consumer, (void *)t);
+	struct threadArgs thread;
+	thread.threadId = t;
+	thread.buffer = buffer;
+        rc = pthread_create(&threads[t], NULL, Consumer, &thread);
         if(rc){
             printf("Error; return code from pthread_create() is %d\n", rc);
-            exit(-1)
+            exit(-1);
         }
     }
-    p = pthread_create(producer, Null, Producer, (void *)requestQ);
+    p = pthread_create(producer, NULL, Producer, (void *)requestQ);
 }
 
 
